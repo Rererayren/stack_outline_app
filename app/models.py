3e31913d13ac3e4cca04fe74add6bc04d78a5446
Rererayren <reneesummer04@gmail.com>
2026-05-15 17:42:20 -0500

    Database Structure Added + Routing

--- app/models.py
+++ app/models.py
@@ -1,9 +1,54 @@
 from .extensions import db
+from datetime import datetime
 
-
-class ExampleRecord(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable=False)
-
+class climber(db.Model):
+    climber_id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    join_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    recent_activity = db.Column(db.DateTime, default=datetime.utcnow)
+    sends = db.relationship("send", back_populates="climber")
+    
     def __repr__(self):
-        return f"<ExampleRecord {self.title}>"
+        return f"<Climber {self.full_name}>"
+    
+class route(db.Model):
+    route_id = db.Column(db.Integer, primary_key=True)  
+    route_name = db.Column(db.String(120), nullable=False)
+    grade_id = db.Column(db.String(20),  db.ForeignKey("grade.grade_id"), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey("location.location_id"), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    grade = db.relationship("grade", back_populates="routes")
+    location = db.relationship("location", back_populates="routes")
+    
+    def __repr__(self):
+        return f"<Route {self.route_name}>"
+    
+class send(db.Model):
+    send_id = db.Column(db.Integer, primary_key=True)
+    climber_id = db.Column(db.Integer, db.ForeignKey("climber.climber_id"), nullable=False)
+    route_id = db.Column(db.Integer, db.ForeignKey("route.route_id"), nullable=False)
+    send_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    entry_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    climber = db.relationship("climber", back_populates="sends")
+    route = db.relationship("route", back_populates="sends")
+    
+    def __repr__(self):
+        return f"<Send {self.send_id}>"
+    
+class location(db.Model):
+    location_id = db.Column(db.Integer, primary_key=True)
+    location_name = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    routes = db.relationship("route", back_populates="location")
+    
+    def __repr__(self):
+        return f"<Location {self.location_name}>"
+    
+class grade(db.Model):
+    grade_id = db.Column(db.Integer, primary_key=True)
+    grade_name = db.Column(db.String(120), nullable=False)
+    routes = db.relationship("route", back_populates="grade")
+    
+    def __repr__(self):
+        return f"<Grade {self.grade_name}>"
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,16 @@
 from flask import Blueprint, render_template
 
-from .models import ExampleRecord
+from .models import climber, route, send, location, grade
 
 main = Blueprint("main", __name__)
 
 
 @main.route("/")
 def index():
-    records = ExampleRecord.query.order_by(ExampleRecord.id.desc()).all()
-    return render_template("index.html", records=records)
+    # records = ExampleRecord.query.order_by(ExampleRecord.id.desc()).all()
+    climbers = climber.query.order_by(climber.climber_id.desc()).all()
+    routes = route.query.order_by(route.route_id.desc()).all()
+    sends = send.query.order_by(send.send_id.desc()).all()
+    locations = location.query.order_by(location.location_id.desc()).all()
+    grades = grade.query.order_by(grade.grade_id.desc()).all()
+    return render_template("index.html", climbers=climbers, routes=routes, sends=sends, locations=locations, grades=grades)

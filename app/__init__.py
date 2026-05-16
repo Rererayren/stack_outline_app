7e56bef2470e0148c82d470b8b3d7c273d1611f5
Rererayren <reneesummer04@gmail.com>
2026-05-15 22:58:23 -0500

    CRUD Requirements + Basic Interface

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 
 from config import Config
 from .extensions import db
-
+from .models import climber, route, send, location, grade
+from datetime import datetime
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -16,5 +17,32 @@ def create_app(config_class=Config):
 
     with app.app_context():
         db.create_all()
+        #check if lookup tables are empty to upload starting data
+        if not grade.query.first() and not location.query.first():
+            grade510a = grade(grade_name="5.10a")
+            grade510b = grade(grade_name="5.10b")
+            grade511a = grade(grade_name="5.11a")
+            grade511b = grade(grade_name="5.11b")
+            grade512a = grade(grade_name="5.12a")
+            grade512b = grade(grade_name="5.12b")
 
+            db.session.add_all([grade510a, grade510b, grade511a, grade511b, grade512a, grade512b])  
+
+            yosemite = location(location_name="Yosemite", state="CA")
+            joshuaTree = location(location_name="Joshua Tree", state="CA")
+            redRock = location(location_name="Red Rock", state="NV")
+            redRiverGorge = location(location_name="Red River Gorge", state="KY")
+            db.session.add_all([yosemite, joshuaTree, redRock, redRiverGorge])  
+
+            db.session.commit()
+
+            route1 = route(route_name="El Capitan", grade_id=grade512b.grade_id, location_id=yosemite.location_id, date_created=datetime.utcnow())
+            route2 = route(route_name="Serenity Crack", grade_id=grade510b.grade_id, location_id=yosemite.location_id, date_created=datetime.utcnow())
+            route3 = route(route_name="Illusion Dweller", grade_id=grade510b.grade_id, location_id=joshuaTree.location_id, date_created=datetime.utcnow())
+            route4 = route(route_name="Triassic Sands", grade_id=grade510a.grade_id, location_id=redRock.location_id, date_created=datetime.utcnow())
+            route5 = route(route_name="Banshee", grade_id=grade511a.grade_id, location_id=redRiverGorge.location_id, date_created=datetime.utcnow())
+            db.session.add_all([route1, route2, route3, route4, route5])
+
+            db.session.commit()
+            
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request, redirect, url_for
 from .models import climber, route, send, location, grade
+from .extensions import db
+from datetime import datetime
 
 main = Blueprint("main", __name__)
 
@@ -13,4 +14,65 @@ def index():
     sends = send.query.order_by(send.send_id.desc()).all()
     locations = location.query.order_by(location.location_id.desc()).all()
     grades = grade.query.order_by(grade.grade_id.desc()).all()
+
     return render_template("index.html", climbers=climbers, routes=routes, sends=sends, locations=locations, grades=grades)
+
+#create POST for creating new climber via user input
+@main.route("/climbers/create", methods=["POST"])
+def create_climber():
+    full_name_input = request.form.get("full_name")
+    email_input = request.form.get("email")
+    
+    #server-side validation to ensure no "bad data" enters database
+    if not full_name_input or not email_input: 
+        return redirect(url_for("main.index")) #fail  
+    try:
+        new_climber = climber(full_name=full_name_input, email=email_input, join_date = datetime.utcNow(), recent_activity = datetime.utcNow())
+        db.session.add(new_climber)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback() #ATOMIC Database Transaction principle
+        print(f"failed to create user: {e}")
+        
+    return redirect(url_for("main.index"))
+
+#transaction logic here for example
+
+@main.route("/trackSend", methods=["POST"])
+def trackSend():
+    climber_id = request.form.get("climber_id")
+    route_id = request.form.get("route_id")
+    
+    if not climber_id or not route_id:
+        return redirect(url_for("main.index"))
+    try:
+        #get climbers information to update relational tables for send information
+        active_climber = climber.query.get(climber_id)
+        
+        #create send record for new ascent entry
+        new_send = send(climber_id=int(climber_id), route_id=int(route_id), send_date=datetime.utcNow(), entry_date=datetime.utcNow())
+        db.session.add(new_send)
+        
+        # need to update the recent activity metadata in the climber table to reflect latest activity
+        if active_climber:
+            active_climber.recent_activity = datetime.utcNow()
+        db.session.commit()
+
+    except Exception as e:
+        db.session.rollback()
+        print(f"failed to log send: {e}")
+    return redirect(url_for("main.index"))
+
+#delete an entry of a send by a climber by send_id
+@main.route("/delete_send/<int:send_id>", methods=["POST"])
+def delete_send(send_id):
+
+    removeSendEntry = send.query.get(send_id)
+    try:
+        db.session.delete(removeSendEntry)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(f"Failed to remove: {e}")
+        
+    return redirect(url_for("main.index"))
